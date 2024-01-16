9c6b63c82b637eaf0b6eb9473ccd8b51cb4b1498
Sarath Kumar S <116628633+SarathKumarS000@users.noreply.github.com>
2024-01-16 13:44:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,19 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import {store, persistor} from './src/components/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
-AppRegistry.registerComponent(appName, () => App);
+
+// AppRegistry.registerComponent(appName, () => App);
+ 
+const ReduxApp = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
+AppRegistry.registerComponent(appName, () => ReduxApp);
+ 
